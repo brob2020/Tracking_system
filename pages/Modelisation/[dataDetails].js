34b4d4817807457b6bee13e0668756cf0b5e3762
brob2020 <Robert.Founang@xerox.com>
2021-06-15 17:41:20 -0300

    making work

--- pages/Modelisation/[dataDetails].js
+++ pages/Modelisation/[dataDetails].js
@@ -1,47 +1,15 @@
-import React from 'react';
-
-/// let tell the broswer how many page he wil load
-
-/*
-export const getStaticPaths = async () => {
-  //const res = await fetch("http://localhost:3000/api/sendData");
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
-
-  /*
-  const paths = data.map((ninja) => {
-    //console.log(data.Incident.toLowerCase());
-    return {
-      params: { id: ninja.id.toString() },
-    };
-  });
-  //console.log(paths);
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async (context) => {
-  const id = context.params.id;
-  const res = await fetch(`http://localhost:3000/api/sendData/${id}`);
-  const data = await res.json();
-  return {
-    props: { data: data },
-  };
-};
-*/
+import React from "react";
 
 export const getStaticPaths = async () => {
   return {
     paths: [], //indicates that no page needs be created at build time
-    fallback: 'blocking', //indicates the type of fallback
+    fallback: "blocking", //indicates the type of fallback
   };
 };
 
 export async function getStaticProps(context) {
   const res = await fetch(
-    `http://localhost:3000/api/data/${context.params.dataDetails.trim()}`,
+    `http://localhost:3000/api/data/${context.params.dataDetails.trim()}`
   );
   const data = await res.json();
   return {
